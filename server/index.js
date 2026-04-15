d217e3680b8be2a7fc2626cb2fdb72a02faaa6ee
PKS Anderson <prakashmonstar4@gmail.com>
2026-04-16 01:14:44 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 // server/index.js
-require('dotenv').config()
+require("dotenv").config();
 const express  = require('express')
 const mongoose = require('mongoose')
 const cors     = require('cors')
